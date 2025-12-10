ab6c6c003d673064faf37af8e55ca03900e523f2
Cuong Ngo <133584912+cuongngodev@users.noreply.github.com>
2025-12-10 13:23:57 -0500

    Update PlayerStateName.js

--- src/enums/PlayerStateName.js
+++ src/enums/PlayerStateName.js
@@ -2,6 +2,7 @@ const PlayerStateName = {
 	Idle: 'idle',
 	Walking: 'walking',
 	SwordSwinging: 'sword-swinging',
+	PerformingFireFlame: 'performing-fire-flame'
 };
 
 export default PlayerStateName;
