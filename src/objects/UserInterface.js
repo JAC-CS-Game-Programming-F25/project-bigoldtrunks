6b28167eeb26ea25014c29fec8b716abbce91b85
linyueCS-IT <6197673@johnabbottcollege.net>
2025-12-12 09:19:30 -0500

    feat: Add health in interface

--- src/objects/UserInterface.js
+++ src/objects/UserInterface.js
@@ -1,4 +1,22 @@
 export default class UserInterface {
-  constructor() {}
-  render() {}
+  constructor(player, region) {
+    this.player = player;
+    this.region = region;
+  }
+  render() {
+    this.renderHealth();
+    this.renderScore();
+  }
+  renderHealth() {
+    context.fillStyle = "#FF0000";
+    context.font = `12px ${FontName.MedievalSharp}`;
+    context.textAlign = "left";
+
+    // use heart to represent health
+    let hearts = "";
+    for (let i = 0; i < this.player.health; i++) {
+      hearts += "❤️";
+    }
+    context.fillText(hearts, 10, 15);
+  }
 }
