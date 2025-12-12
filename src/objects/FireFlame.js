bda1dbc665a6b197f42d5552f5c4b6a30b7c3fe8
Cuong Ngo <133584912+cuongngodev@users.noreply.github.com>
2025-12-11 21:22:59 -0500

    Update FireFlame to get currentFrame from animation:
    - modify dimension

--- src/objects/FireFlame.js
+++ src/objects/FireFlame.js
@@ -5,8 +5,8 @@ import { images } from "../globals.js";
 import GameObject from "./GameObject.js";
 
 export default class FireFlame extends GameObject {
-    static WIDTH = 32;
-    static HEIGHT = 32;
+    static WIDTH = 28;
+    static HEIGHT = 28;
 
     constructor(position, direction) {
         super(
@@ -19,15 +19,18 @@ export default class FireFlame extends GameObject {
             FireFlame.WIDTH,
             FireFlame.HEIGHT        
         );
-        this.animation = new Animation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0.4, 1);
+        this.animation = new Animation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0.05, 1);
         this.currentAnimation = this.animation;  // Store the Animation object, not the frame number
+        this.currentFrame = 0;  // Initialize current frame
 
         console.log("FireFlame created at position:", position, "with direction:", direction);
     }
 
     update(dt) {
         // Update the animation
+        this.currentFrame = this.currentAnimation.getCurrentFrame();
         super.update(dt);
+        // Update the current frame for rendering
     }
 
     render(context) {
