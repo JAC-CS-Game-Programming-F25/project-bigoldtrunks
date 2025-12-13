4addbab3c864b64b40377c9b4ce7a692c6c971ea
linyueCS-IT <6197673@johnabbottcollege.net>
2025-12-13 01:08:15 -0500

    feat: create victory state

--- src/states/VictoryState.js
+++ src/states/VictoryState.js
@@ -1,7 +1,20 @@
 import State from "../../lib/State.js";
+import { input } from "../globals.js";
+import Input from "../../lib/Input.js";
 
 export default class VictoryState extends State {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
+
+  update(dt) {
+    // Press Enter to go back to title
+    if (input.isKeyPressed(Input.KEYS.ENTER)) {
+      keys.Enter = false;
+      stateMachine.change(GameStateName.Transition, {
+        fromState: this,
+        toState: stateMachine.states[GameStateName.TitleScreen],
+      });
+    }
+  }  
 }
