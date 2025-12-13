5d7cef9a16003da727797eecaa21c754508f5684
linyueCS-IT <6197673@johnabbottcollege.net>
2025-12-12 20:48:49 -0500

    feat: add constructor

--- src/entities/Creature/BigBoss.js
+++ src/entities/Creature/BigBoss.js
@@ -1,10 +1,27 @@
+import Direction from "../../enums/Direction.js";
+import Vector from "../../../lib/Vector.js";
 export default class BigBoss extends Creature {
+  static WIDTH = 128;
+  static HEIGHT = 128;
+  static SPEED = 25;
+  static HEALTH = 500;
   constructor(position) {
-    super();
+    super({
+      position,
+      speed: BigBoss.SPEED,
+      health: BigBoss.HEALTH,
+      damage: 2,
+      canChase: true,
+      dimensions: new Vector(BigBoss.WIDTH, BigBoss.HEIGHT),
+    });
+    this.direction = Direction.Left;
+    this.loadSprites();
+    this.setupAnimations();
+    this.stateMachine = this.initializeStateMachine();
   }
 
-  update() {}
   loadSprites() {}
+  update() {}
   setupAnimations() {}
   initializeStateMachine() {}
   render() {}
