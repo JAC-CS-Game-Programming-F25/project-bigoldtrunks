a28971daabc811a35ccb38a0e1792a419c600126
linyueCS-IT <6197673@johnabbottcollege.net>
2025-12-14 12:26:58 -0500

    feat: create new summer map

--- assets/maps/tree-tiles-winter.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tree-tiles-winter" tilewidth="16" tileheight="16" tilecount="168" columns="12">
- <image source="../images/Tiles/Trees/Model_02/Size_04.png" width="192" height="224"/>
-</tileset>
